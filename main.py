8abf5a4d2920a3e4eacf70ff2b97d139755a45a7
Jeremi Pamula <jeremipamula@gmail.com>
2025-01-17 10:51:00 +0100

    implemented recursive function to get training data from network

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
 import copy
 
+import miniMax as mm
 import hexaPawn
-def minimax(boardState, move, currDepth, targetDepth,alpha=-1000,beta=1000):
-    board=copy.deepcopy(boardState)
-    board.applyMove(move)
-    if board.isTerminal()[0] or currDepth==targetDepth:
-        return board.isTerminal()[1]
-    if board.turn==1:
-        best=-1000
-        for move in board.generateMoves():
-            evaluation=minimax(board, move, currDepth + 1, targetDepth,alpha,beta)
-            best=max(evaluation,best)
-        return best
-    if board.turn==-1:
-        best=1000
-        for move in board.generateMoves():
-            evaluation = minimax(board, move, currDepth + 1, targetDepth,alpha,beta)
-            best = min(evaluation, best)
-        return best
+from miniMax import mini_max
+
+# from os import environ
+# environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+# import keras
+# from keras.src.layers import Dense
+# from keras import Input
+
+outcomes={'position':[],
+          'move':[],
+          'winner':[]}
 board=hexaPawn.Board()
 board.setStartingPosition()
-print(board.getPosition())
-print(board.toNetworkInput())
-print(board.getPosition())
-print('ruchy',board.generateMoves(),board.turn)
-board.applyMove((7,4))
-print(board.getPosition())
-print('ruchy',board.generateMoves(),board.turn)
-print(minimax(board, board.generateMoves()[3], 0, 10))
+def get_training_data(board,outcomes):
+    if board.isTerminal()[0]:
+        return
+    for move in board.generateMoves():
+        outcomes['position'].append(board.getPosition())
+        outcomes['move'].append(move)
+        outcomes['winner'].append(mini_max(board, move, 0, 5))
+        temp=copy.deepcopy(board)
+        temp.applyMove(move)
+        get_training_data(temp,outcomes)
+get_training_data(board,outcomes)
 
-# board.applyMove(board.generateMoves()[0])
-# print('ruchy',board.generateMoves(),board.turn)
-# board.applyMove(board.generateMoves()[0])
-# print('ruchy',board.generateMoves(),board.turn)
-# print(board.getPosition(),board.generateMoves(),board.turn)
-# board.applyMove((8,5))
-# print('ruchy2',board.generateMoves(),board.turn,board.isTerminal())
+
 
